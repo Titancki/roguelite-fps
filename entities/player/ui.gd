108736821bd7eb396f08e0ac6b0388a9a91c9d3b
User <user@MacBook-Pro-de-User.local>
2025-01-14 18:59:01 +0100

    fix scaling feat  weapon ui

--- entities/player/ui.gd
+++ entities/player/ui.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var reload_delay : Timer = $"../ReloadDelay"
 @onready var player : CharacterBody3D = get_parent()
-@onready var gun_delay_bar := $Gun/Cooldown_bar
+@onready var gun_delay_bar := $Weaponpanel/Cooldown_bar
 @onready var pause_menu = $"Pause_menu"
 @onready	 var weapon_sprite = $WeaponSprite
 
@@ -16,7 +16,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	change_reticule()
-	$Gun/Ammo_txt.set_text(str(snapped(player.current_gun_ammo, 1)))
+	$Weaponpanel/Ammo_txt.set_text(str(snapped(player.current_gun_ammo, 1)))
 
 	if not reload_delay.is_stopped():
 		gun_delay_bar.value = 1.0 - reload_delay.time_left / reload_delay.wait_time
@@ -33,8 +33,8 @@ func _process(delta: float) -> void:
 		else:
 			weapon_sprite.play("hold")
 	
-				
-		
+	update_to_viewport_size()
+	
 func _input(event):
 	if Input.is_action_just_pressed("pause"):
 		toggle_pause()
@@ -72,3 +72,29 @@ func toggle_pause():
 
 func _on_weapon_sprite_animation_finished() -> void:
 	can_anim_change = true
+
+func scroll_weapon_ui() -> void:
+	var weapon_index = (player.weapon_inventory.weapon_list.find(player.current_weapon) + 1) % player.weapon_inventory.weapon_list.size()
+	var panel = $Weaponpanel/FillPanel
+	print(weapon_index)
+	
+	# Load the texture based on weapon index or weapon type
+	var texture_path = ""
+	match weapon_index:
+		1:
+			texture_path = "res://assets/sprites/ui weapon fill1.png"
+		2:
+			texture_path = "res://assets/sprites/ui weapon fill2.png"
+		3:
+			texture_path = "res://assets/sprites/ui weapon fill3.png"
+		_:
+			texture_path = "res://assets/sprites/ui weapon fill3.png"
+	
+	var texture = load(texture_path)
+	if texture:
+		var new_stylebox = StyleBoxTexture.new()
+		new_stylebox.texture = texture
+		panel.add_theme_stylebox_override("panel", new_stylebox)
+
+func update_to_viewport_size():
+	weapon_sprite.scale = Vector2(get_viewport().size.x / 480, get_viewport().size.y / 270)
